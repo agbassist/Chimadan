050d2cb48750249a8e471d0db80f41847087f0f7
Yarden Tamir <ytamir18@gmail.com>
2017-03-29 18:33:40 -0500

    Make sure comments make sense

--- src/libpriqueue/libpriqueue.c
+++ src/libpriqueue/libpriqueue.c
@@ -20,6 +20,7 @@
  */
 void priqueue_init(priqueue_t *q, int(*comparer)(const void *, const void *))
 {
+        /* constructer which sets the parametes given */
         q->size = 0;
         q->head = NULL;
         q->comparer = comparer;
@@ -35,30 +36,38 @@ void priqueue_init(priqueue_t *q, int(*comparer)(const void *, const void *))
  */
 int priqueue_offer(priqueue_t *q, void *ptr)
 {
+        /* Checks if Head is empty */
         if(q->head == NULL)
         {
+                /* Create a Head */
                 q->head = malloc(sizeof(task_t));
                 q->head->ptr = ptr;
                 q->head->next = NULL;
         }
+        /* The Queue is not empty */
         else
         {
+                /* Create a temp variable for the old head */
                 task_t * temp = q->head;
                 task_t * placeholder = q->head;
 
+                /* Loop through Queue */
                 while(temp != NULL)
                 {
+                        /* Compare values */
                         if(q->comparer(ptr,temp->ptr) < 0)
                         {
-
+                                /* Check if the result is the head */
                                 if(temp == q->head)
                                 {
+                                        /* Create task_t object */
                                         q->head = malloc(sizeof(task_t));
                                         q->head->ptr = ptr;
                                         q->head->next = temp;
                                 }
                                 else
                                 {
+                                        /* Create task_t object */
                                         placeholder->next = malloc(sizeof(task_t));
                                         placeholder = placeholder->next;
                                         placeholder->ptr = ptr;
@@ -66,13 +75,14 @@ int priqueue_offer(priqueue_t *q, void *ptr)
                                 }
                                 break; //if the right place is found, no need to continue while loop
                         }
+                        /* Increment the place in the loop and have a rider that sits one back */
                         placeholder = temp;
                         temp = temp->next;
                 }
-
+                /* If it gets to the end of the queue */
                 if(temp == NULL)
                 {
-                        //if it gets to the end of the queue
+                        /* Create task_t object */
                         temp = malloc(sizeof(task_t));
                         temp->ptr = ptr;
                         temp->next = NULL;
@@ -109,20 +119,26 @@ void *priqueue_peek(priqueue_t *q)
  */
 void *priqueue_poll(priqueue_t *q)
 {
+        /* Create temp of the Head */
         task_t * temp = q->head;
+        /* If the Queue is empty */
         if(temp == NULL)
         {
                 return NULL;
         }
+        /* If the Queue is not empty */
         else
         {
+                /* Create temp of the Head */
                 task_t * temp = q->head;
+                /* If Head the only object in the Queue */
                 if(temp->next != NULL)
                 {
                         q->head = temp->next;
                 }
                 else
-                { //size of queue is 1
+                /* If the Size of the Queue is 1 */
+                {
                         q->head = NULL;
                 }
                 void* data = temp->ptr;
@@ -132,7 +148,6 @@ void *priqueue_poll(priqueue_t *q)
                 return(data);
         }
 }
-
 
 /**
   Returns the element at the specified position in this list, or NULL if
@@ -146,14 +161,16 @@ void *priqueue_poll(priqueue_t *q)
 void *priqueue_at(priqueue_t *q, int index)
 {
 
+        /* if the index is at the head */
         if(index==0)
         {
                 return q->head->ptr;
         }
 
+        /* Create a temp task of the head */
         task_t * temp = q->head;
         int count = 0;
-
+        /* Loop until the right value */
         while(temp != NULL && count < index)
         {
                 temp = temp->next;
@@ -175,32 +192,38 @@ void *priqueue_at(priqueue_t *q, int index)
  */
 int priqueue_remove(priqueue_t *q, void *ptr)
 {
+        /* If the Queue is empty */
         if(q->head == NULL)
         {
                 return 0;
         }
+        /* If the Queue is not empty */
         else
         {
+                /* Create two temps for the Head */
                 task_t * temp = q->head; //used to traverse queue
                 task_t * prev = q->head; //stores the previous temp value, used for reassigning next values
                 int count = 0; //used to count how many have been removed
-
+                /* while temp is not at the end */
                 while(temp != NULL)
-                { //while temp is not at the end
-
-
+                {
+                        /* If we have a match */
                         if(temp->ptr == ptr)
-                        { //if we have a match
-
+                        {
+                                 /* if the head is a match */
                                 if(temp == q->head)
-                                { //if the head is a match
-                                        q->head = q->head->next; //reset the head
+                                {
+                                        /* reset the head */
+                                        q->head = q->head->next;
                                 }
                                 else
                                 {
+                                        /* Continue Looping */
                                         prev->next = temp->next;
                                 }
+                                /* Free the temp task */
                                 free(temp);
+                                /* Change the head */
                                 temp = q->head;
                                 prev = q->head;
                                 q->size--;
@@ -208,6 +231,7 @@ int priqueue_remove(priqueue_t *q, void *ptr)
                         }
                         else
                         {
+                        /* Increment the Loop */
                         prev = temp;
                         temp = temp->next;
                         }
@@ -231,17 +255,21 @@ int priqueue_remove(priqueue_t *q, void *ptr)
  */
 void *priqueue_remove_at(priqueue_t *q, int index)
 {
+        /* Create two temps for the head of the Queue */
         task_t * temp = q->head;
         task_t * prev = q->head;
         int count = 0;
 
+        /* Loop until the right index */
         while(temp != NULL && count <= index)
         {
+                /* Increment the values for the loop */
                 prev = temp;
                 temp = temp->next;
                 count = count + 1;
         }
 
+        /* If the node to remove is the head of the Queue */
         if(temp == q-> head)
         {
                 q->head = temp->next;
@@ -275,29 +303,38 @@ int priqueue_size(priqueue_t *q)
  */
 void priqueue_destroy(priqueue_t *q)
 {
+        /* Loop through the Queue Deleting each one */
         while(q->head != NULL)
         {
                 priqueue_poll(q);
         }
 }
 
+/**
+  Prints what is in the Queue
+
+  @param q a pointer to an instance of the priqueue_t data structure
+ */
 void priqueue_print(priqueue_t *q)
 {
-        
+        /* Create a temp for the head of the */
         task_t* temp = q->head;
 
+        /* If the Queue is not empty */
         if(temp != NULL)
         {
                 printf("[");
-
+                /* Loop inside of the head to print out the contents of the Queue */
                 while(temp != NULL)
                  {
                         if(temp->next != NULL)
                         {
+                                /* Print the contents of the Queue */
                                 printf("%i, ", *(int *)temp->ptr);
                         }
                         else
                         {
+                                /* Print the contents of the Queue */
                                 printf("%i]\n", *(int *)temp->ptr);
                                 temp = temp->next;
                         }
@@ -305,6 +342,7 @@ void priqueue_print(priqueue_t *q)
         }
         else
         {
+                /* Print an empty Queue */ 
                 printf("[]");
         }
 }
--- src/libscheduler/libscheduler.c
+++ src/libscheduler/libscheduler.c
@@ -12,80 +12,119 @@
 
 
 /**
-  Stores information making up a job to be scheduled including any statistics.
-
-  You may need to define some global variables or a struct to store your job queue elements.
+  Name : job_t
+  Description :Stores information making up a job to be scheduled including any statistics.
 */
-
 
 typedef struct _job_t
 {
-  int job_number;       //job id
-  int arrival_time;     //time arrived at the queue
-  int priority;         //priority of the job
-  int runtime;          //total time for the job to complete
-  int time_remaining;   //time remaining before the job finishes
-  int start_time;       //time that the job is added to the core and started
-  int response;         //time that the job first has a response
-  bool flag;            //checks whether the job has used any cpu time
+  int job_number;       /* job id */
+  int arrival_time;     /* time arrived at the queue */
+  int priority;         /* priority of the job */
+  int runtime;          /* total time for the job to complete */
+  int time_remaining;   /* time remaining before the job finishes */
+  int start_time;       /* time that the job is added to the core and started */
+  int response;         /* time that the job first has a response */
+  bool flag;            /* checks whether the job has used any cpu time */
 
 } job_t;
 
-priqueue_t Q;
-job_t ** corearr; // job task array
-int Nums[100];
-int num = 0;
+priqueue_t Q; /* Queue for the Program */
+job_t ** corearr; /* Array for the cores in the program */
 
+/*
+Name: ShortestJobFirst(const void * x, const void * y)
+
+Description: Sorts the provided jobs by which is the shortest
+
+Return: Returns a 1 if the time remaining is greater in the second job
+  and returns a -1 if the time remaining is the is greater in the fist job
+  and returns a 0 if the time remaining is the same for both Jobs
+
+ */
 int ShortestJobFirst(const void * x, const void * y)
 {
+        /* returns a 1 if the time remaining is greater in the second job */
         if( (*(job_t*)x).time_remaining > (*(job_t*)y).time_remaining) //if the first arrived later return the second
         {
                 return 1;
         }
+        /* returns a -1 if the time remaining is the is greater in the fist job */
         else if ((*(job_t*)x).time_remaining < (*(job_t*)y).time_remaining)// if the first arrived earlier return the first
         {
                 return -1;
         }
         else
+        /* returns a 0 if the time remaining is the same for both Jobs */
         {
                 return 0;
         }
 }
 
+/*
+Name: PriorityFirst(const void * x, const void * y)
+
+Description: Sorts the provided jobs by which has the greater priority
+
+Return: Returns a 1 if the priority is greater in the second job
+  and returns a -1 if the priority is the is greater in the fist job
+  and returns a 0 if the priority is the same for both Jobs
+
+ */
 int PriorityFirst(const void * x, const void * y)
 {
-        if( (*(job_t*)x).priority > (*(job_t*)y).priority) //If the first has a larger priority value, return the second
+        if( (*(job_t*)x).priority > (*(job_t*)y).priority)
+        /* If the first has a larger priority value, return the second */
         {
                 return 1;
         }
-        else if ( (*(job_t*)x).priority < (*(job_t*)y).priority)// If the first has a lower priority value, return the first
+        else if ( (*(job_t*)x).priority < (*(job_t*)y).priority)
+        /* If the first has a lower priority value, return the first */
         {
                 return -1;
         }
         else
         {
-                if( (*(job_t*)x).arrival_time > (*(job_t*)y).arrival_time) //if the first arrived later return the second
+                if( (*(job_t*)x).arrival_time > (*(job_t*)y).arrival_time)
+                /* if the first arrived later return the second */
                 {
                         return 1;
                 }
-                else if ((*(job_t*)x).arrival_time < (*(job_t*)y).arrival_time)// if the first arrived earlier return the first
+                else if ((*(job_t*)x).arrival_time < (*(job_t*)y).arrival_time)
+                /* if the first arrived earlier return the first */
                 {
                         return -1;
                 }
                 else
+                /*returns a zero if the priority and then arrival time are the same */
                 {
                 return 0;
                 }
         }
 }
 
+/*
+Name: FirstComeFirstServe(const void * a, const void * b)
+
+Description: Sorts the provided jobs by which came first
+
+Return: Returns a 1
+*/
 int FirstComeFirstServe(const void * a, const void * b)
 {
         return 1;
 }
 
+/*
+Name: hasjobfinished(job_t * job)
+
+Description: Returns whether the job has finished
+
+Return: Returns true if remaining_time is 0, else a false
+*/
 bool hasjobfinished(job_t * job)
 {
+        /* Returns true if remaining_time is 0, else a false */
         if (job->time_remaining == 0)
         {
                 return true;
@@ -96,23 +135,37 @@ bool hasjobfinished(job_t * job)
         }
 }
 
+/*
+Name: scheduler_core_available(job_t * newjob)
+
+Description: Returns whether there is a core that is idle
+
+Return: Returns the index of an avaliable core
+        else, returns -1
+*/
 int scheduler_core_available(job_t* newjob)
 {
         int a = 0;
+        /* Loops to check if there is a core avaliable */
         while( a < ncores  )
         {
+                /* Checks if the core is idle */
                 if(corearr[a] ==  NULL)
                 {
                         corearr[a] = newjob;
+                        /* Returns the index of the available core */
                         return a;
                 }
                 a++;
         }
+        /* If no core is available then return -1 */
         return -1;
 }
 
 
 /**
+  Name: scheduler_start_up(int cores, scheme_t scheme)
+
   Initalizes the scheduler.
 
   Assumptions:
@@ -127,7 +180,7 @@ int scheduler_core_available(job_t* newjob)
 void scheduler_start_up(int cores, scheme_t scheme)
 {
 
-        //assign the type of comprison with the initialization of the queue
+        /*assign the type of comprison with the initialization of the queue*/
         if(scheme == FCFS)
         {
                 priqueue_init(&Q, FirstComeFirstServe);
@@ -153,27 +206,32 @@ void scheduler_start_up(int cores, scheme_t scheme)
                 priqueue_init(&Q, PriorityFirst);
         }
 
+        /* Initialize Global variables to 0 */
         wait_time = 0;
         turnaround_time = 0;
         response_time =0;
         nJobs = 0;
         type = scheme;
 
+        /* Initialize the number of cores to the cores parameter */
         ncores = cores;
+        /* Create an int holding the size of a job_t */
         int sojt = sizeof(job_t);
+        /* Create an int to hold the amount of total alloment for cores that will be needed */
         int allocamount = ncores *sojt;
+        /* Allacote an array of job_t to corearr */
         corearr = (job_t**)malloc(allocamount);
         int a;
+        /* Loop through the amount of cores and set them in corearr to NULL */
         for (a =0; a < ncores; a++)
         {
                 corearr[a] = NULL;//NULL
         }
-
-//priqueue_init(Queue);
 }
 
-
 /**
+  Name: scheduler_new_job(int job_number, int time, int running_time, int priority)
+
   Called when a new job arrives.
 
   If multiple cores are idle, the job should be assigned to the core with the
@@ -195,7 +253,9 @@ void scheduler_start_up(int cores, scheme_t scheme)
  */
 int scheduler_new_job(int job_number, int time, int running_time, int priority)
 {
+        /* Create a newjob job_t */
         job_t* newjob = malloc(sizeof(job_t));
+        /* Set the values of the parameters to the parameters inside of newjob */
         newjob->priority = priority;
         newjob->runtime = running_time;
         newjob->time_remaining = running_time;
@@ -204,62 +264,66 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority)
         newjob->job_number = job_number;
         newjob->response = INT_MIN;
 
+        /* Create an int to house if there is available core */
         int resp = scheduler_core_available(newjob);
+        /* If there is a core available */
         if (resp != -1 )
         {
                 newjob->response = 0;
+                /* returns the index of the core that newjob was placed inside */
                 return resp;
-        //the new job has been added
         }
+        /* If there is no core available */
         else
         {
                 if(type == PSJF)
                 {
-                //Find the core with the max remaining time
+                        /* Find the core with the max remaining time */
                         int max_remaining_time = INT_MIN;
                         int index_of_max = 0;
+                        /* Loops through the cores */
                         for(int i=0; i<ncores;i++)
-                        {//for each core
-                                //Update the remaining time for each core to find max
+                        {
+                                /*Update the remaining time for each core to find max */
                                 corearr[i]->time_remaining -= (time - corearr[i]->start_time);
 
+                                /* If a new max is found, update location and value of max */
                                 if(corearr[i]->time_remaining > max_remaining_time)
                                 {
-                                        //If a new max is found, update location and value of max
                                         max_remaining_time = corearr[i]->time_remaining;
                                         index_of_max = i;
                                 }
                         }
-                        //If the new job's remaining time is less than the chose core
+                        /* If the new job's remaining time is less than the chose core */
                         if(newjob->time_remaining < corearr[index_of_max]->time_remaining)
                         {
-                                //Add the currently executing job to the queue
+                                /* Add the currently executing job to the queue */
                                 priqueue_offer(&Q, corearr[index_of_max]);
-                                //Put the new job in the core
+                                /* Put the new job in the core */
                                 newjob->response = 0;
                                 corearr[index_of_max] = newjob;
                                 return index_of_max;
                         }
-
                 }
-
                 if (type == PPRI)
                 {
-                        //Find the core with the max remaining time
+                        /* Find the core with the max remaining time */
                         int max_priority_num = INT_MIN;
                         int index_of_max = 0;
+                        /* Loops through the cores */
                         for(int i=0; i<ncores;i++)
-                        {//for each core
+                        {
 
-                                //Update the remaining time for each core (need to do this to calculate response time)
+                                /* Update the remaining time for each core (need to do this to calculate response time) */
                                 corearr[i]->time_remaining -= (time - corearr[i]->start_time);
 
                                 if(corearr[i]->priority > max_priority_num)
                                 {
-                                        //If a new max is found, update location and value of max
+                                        /* If a new max is found, update location and value of max */
                                         max_priority_num = corearr[i]->priority;
                                         index_of_max = i;
                                 }
+                                /* If the priorities are the same then check the arrival time */
                                 else if(corearr[i]->priority == max_priority_num)
                                 {
                                         if(corearr[i]->arrival_time > corearr[index_of_max]->arrival_time)
@@ -269,25 +333,26 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority)
                                         }
                                 }
                         }
-                        //If the new job's remaining time is less than the chose core
+                        /* If the new job's remaining time is less than the chose core */
                         if(newjob->priority < corearr[index_of_max]->priority)
                         {
-                                //Add the currently executing job to the queue
+                                /*Add the currently executing job to the queue*/
                                 priqueue_offer(&Q, corearr[index_of_max]);
-                                //Put the new job in the core
+                                /*Put the new job in the core*/
                                 newjob->response = 0;
                                 corearr[index_of_max] = newjob;
                                 return index_of_max;
                         }
                 }
-
         }
+        /* Add the new job to the Queue */
         priqueue_offer(&Q, newjob);
         return -1;
 }
 
 
 /**
+  Name: scheduler_job_finished(int core_id, int job_number, int time)
   Called when a job has completed execution.
 
   The core_id, job_number and time parameters are provided for convenience.
@@ -304,34 +369,30 @@ int scheduler_new_job(int job_number, int time, int running_time, int priority)
  */
 int scheduler_job_finished(int core_id, int job_number, int time)
 {
-        //Grab the finished job from the core
+        /* Grab the finished job from the core */
         job_t* finished_job = corearr[core_id];
 
-        //Calculate the different time measurements from the finished job
+        /* Calculate the different time measurements from the finished job */
         wait_time       += (time - finished_job->runtime - finished_job->arrival_time);
         turnaround_time += (time - finished_job->arrival_time);
         response_time += finished_job->response;
-
-        Nums[num] = finished_job->response;
-        num++;
-
-
+        /* Increment the number of Jobs */
         nJobs++;
 
-        //Cleanup the job
+        /* Cleanup the job */
         free(finished_job);
         finished_job = NULL;
         corearr[core_id] = NULL;
 
-        //Schedule the next job to the core
-
+        /* Schedule the next job to the core */
+        /* Check if the Queue is empty */
         if(priqueue_peek(&Q) != NULL)
-        { //Check if the Queue is empty
-                //Grab the job at the top of the queue and add it to the core
+        {
+                /* Grab the job at the top of the queue and add it to the core */
                 job_t* new_job = (job_t*)priqueue_poll(&Q);
-
                 new_job->start_time = time;
 
+                /* Change the response time if it hasn't been redefined */
                 if(new_job->response == INT_MIN)
                 {
                         new_job->response = time - new_job->arrival_time;
@@ -340,17 +401,21 @@ int scheduler_job_finished(int core_id, int job_number, int time)
                 {
                         new_job->response = time - new_job->arrival_time;
                 }
+                /* Change the job in the core */
                 corearr[core_id] = new_job;
+                /* return the job number for the new job */
                 return new_job->job_number;
         }
+        /* if the Queue is empty, the core remains idle */
         else
-        { //if the Queue is empty, the core remains idle
+        {
                 return -1;
         }
 }
 
 
 /**
+  Name: scheduler_quantum_expired(int core_id, int time)
   When the scheme is set to RR, called when the quantum timer has expired
   on a core.
 
@@ -369,31 +434,31 @@ int scheduler_quantum_expired(int core_id, int time)
         priqueue_offer(&Q,corearr[core_id]);
         job_t * current_job = priqueue_poll(&Q);
 
+        /* Check if the Queue is empty */
         if(current_job != NULL)
-        { //Check if the Queue is empty
+        {
+                /* Add the expired job to the queue, and schedule the next job on the queue */
                 corearr[core_id]->time_remaining -= (time - corearr[core_id]->start_time);
-                //Add the expired job to the queue, and schedule the next job on the queue
-                //remaining
-                //temp_job = priqueue_poll(&Q);
                 current_job->start_time = time;
                 corearr[core_id]= current_job;
 
+                /* If the response variable has not been set yet then redife it */
                 if(corearr[core_id]->response == INT_MIN)
                 {
                         corearr[core_id]->response = time - corearr[core_id]->arrival_time;
                 }
                 return current_job->job_number;
         }
+        /* Return -1 if the Queue is empty and remain idle */
         else
         {
-                //corearr[core_id]->time_remaining -= (time - corearr[core_id]->start_time);
-                //Return -1 if the Queue is empty and remain idle
                 return -1;
         }
 }
 
 
 /**
+  Name: scheduler_average_waiting_time()
   Returns the average waiting time of all jobs scheduled by your scheduler.
 
   Assumptions:
@@ -408,6 +473,7 @@ float scheduler_average_waiting_time()
 
 
 /**
+  Name: scheduler_average_turnaround_time()
   Returns the average turnaround time of all jobs scheduled by your scheduler.
 
   Assumptions:
@@ -422,6 +488,7 @@ float scheduler_average_turnaround_time()
 
 
 /**
+  Name: scheduler_average_response_time()
   Returns the average response time of all jobs scheduled by your scheduler.
 
   Assumptions:
@@ -436,6 +503,7 @@ float scheduler_average_response_time()
 
 
 /**
+  Name: scheduler_clean_up()
   Free any memory associated with your scheduler.
 
   Assumptions:
@@ -444,19 +512,22 @@ float scheduler_average_response_time()
 void scheduler_clean_up()
 {
         int x =0 ;
+        /* Loop through the cores */
         while (x < ncores)
         {
                 if(corearr[x] == NULL)
                 {
-                        //do nothing beacause no job_task inside
+                        /*do nothing beacause no job_task inside */
                 }
                 else
                 {
+                        /* free the core */
                         free(corearr[x]);
-
                 }
                 x++;
         }
+
+        /* Free the corearr and Q structures */
         free(corearr);
         priqueue_destroy(&Q);
 }
